306c533eba9ee6a4c90e913434aec02734af18e4
AliMar11 <alicia.marisal@gmail.com>
2015-12-04 11:29:13 -0500

    methods lab complete^_^

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -5,66 +5,69 @@
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
    
     NSString *palindrome = @"palindrome";
     NSString *reversed = [self stringByReversingString:palindrome];
     NSLog(@"%@ : %@", palindrome, reversed);
     
+    
     NSString *racecar = @"racecar";
     BOOL racecarIsPalindrome = [self stringIsPalindrome:racecar];
     NSLog(@"%d : %@", racecarIsPalindrome, racecar);
     
+    
     BOOL palindromeIsPalindrome = [self stringIsPalindrome:palindrome];
     NSLog(@"%d : %@", palindromeIsPalindrome, palindrome);
  
+    
     NSString *bob = @"Bob";
     BOOL bobIsPalindrome = [self stringIsPalindrome:bob];
     NSLog(@"%d : %@", bobIsPalindrome, bob);
+    
     
     NSString *kanakanak = @"Kanakanak";
     BOOL kanakanakIsPalindrome = [self stringIsPalindrome:kanakanak];
     NSLog(@"%d : %@", kanakanakIsPalindrome, kanakanak);
     
+    
     NSString *aibohphobia = @"Aibohphobia";
     BOOL aibohphobiaIsPalindrome = [self stringIsPalindrome:aibohphobia];
     NSLog(@"%d : %@", aibohphobiaIsPalindrome, aibohphobia);
+    
     
     NSString *notAPalindrome = @"this is not a Palindrome";
     BOOL notAPalindromeIsPalindrome = [self stringIsPalindrome:notAPalindrome];
     NSLog(@"%d : %@", notAPalindromeIsPalindrome, notAPalindrome);
     
+    
     NSString *neverOdd = @"never odd or even";
     BOOL neverOddIsPalindrome = [self stringIsPalindrome:neverOdd];
     NSLog(@"%d : %@", neverOddIsPalindrome, neverOdd);
+    
     
     NSString *iPreferPi = @"I prefer pi";
     BOOL iPreferPiIsPalindrome = [self stringIsPalindrome:iPreferPi];
     NSLog(@"%d : %@", iPreferPiIsPalindrome, iPreferPi);
     
+    
     NSString *fleeToMe = @"Flee to me, remote elf.";
     BOOL fleetyElf = [self stringIsPalindrome:fleeToMe];
     NSLog(@"%d : %@", fleetyElf, fleeToMe);
+    
     
     NSString *selflessNorma = @"Norma is as selfless as I am. Ron.";
     BOOL selflessNormaIsPalindrome = [self stringIsPalindrome:selflessNorma];
     NSLog(@"%d : %@", selflessNormaIsPalindrome, selflessNorma);
     
+    
     NSString *papayaWar = @"No sir! Away! A papaya war is on.";
     BOOL papayaWarIsPalindrome = [self stringIsPalindrome:papayaWar];
     NSLog(@"%d : %@", papayaWarIsPalindrome, papayaWar);
-    
     
 
     // do not alter
     return YES;  //
 }   ///////////////
-
-/**
- 
- * Implement your methods here.
- 
- */
 
 - (BOOL)stringIsPalindrome:(NSString *)string {
     NSArray *punctuations = @[ @".", @"!", @",", @"?", @";", @":" ];
