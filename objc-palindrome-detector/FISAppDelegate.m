9167418a75055e08150dc450dc2e2f4bdcb93c22
dani2906 <danny@yaptapapp.com>
2015-12-30 17:31:53 -0500

    build palindrome program

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -6,21 +6,60 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-
-    /**
-     
-     * Write your check code here.
-     
-     */
     
+    NSString *palindrome = @"Flee to me, remote elf.";
+    
+    NSString *reversed = [self stringByReversingString:palindrome];
+
+     
+    NSLog(@"%@ : %@", palindrome, reversed);
+    
+
+    BOOL palindromeIsPalindrome = [self stringIsPalindrome:palindrome];
+    NSLog(@"%d : %@", palindromeIsPalindrome, palindrome);
+
     // do not alter
     return YES;  //
 }   ///////////////
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+    
+    return result;
+}
+
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
+    NSString *withoutPunctuation = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSString *lowercase = [spaceless lowercaseString];
+    
+    NSString *reverse = [self stringByReversingString:lowercase];
+    
+    NSLog(@"lowercase: %@, reverse: %@", lowercase, reverse);
+    
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
+
+
 
 @end
