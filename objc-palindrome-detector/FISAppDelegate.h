489d7eb06e307879c206c8aba06141a9c4200c97
Elli Scharlin <Elli@Ellis-MacBook-Air.local>
2016-01-13 14:49:02 -0500

    added both the h and the m

--- objc-palindrome-detector/FISAppDelegate.h
+++ objc-palindrome-detector/FISAppDelegate.h
@@ -6,10 +6,8 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+- (BOOL)stringIsPalindrome:(NSString *)string; 
+
+- (NSString *)stringByReversingString:(NSString *)string;
 
 @end
