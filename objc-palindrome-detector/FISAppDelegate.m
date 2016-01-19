adfc9ce55b013fccbd961fc322165c2d930fd589
jimmydg16 <jimmydg16@gmail.com>
2016-01-19 14:22:42 -0500

    Completed Palindrome Detector

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -12,6 +12,66 @@
      * Write your check code here.
      
      */
+// Reversing a string and checking the method return.
+    //NSString *palindrone = @"palindrone";
+    //NSString *reversed = [self stringByReversingString:palindrone];
+    //NSLog(@"%@ : %@", palindrone, reversed);
+    
+// Detecting a Palindrome.
+    //NSString *racecar = @"racecar";
+    //BOOL racecarIsPalindrome = [self stringIsPalindrome:racecar];
+    //NSLog(@"%d : %@", racecarIsPalindrome, racecar);
+    
+    //NSString *palindrone =@"palindrone";
+    //BOOL palindroneIsPalindrone = [self stringIsPalindrome:palindrone];
+    //NSLog(@"%D : %@", palindroneIsPalindrone, palindrone);
+    
+    
+    //single word, with uppercase
+    NSString *bob = @"Bob";
+    BOOL bobIsPalindrome = [self stringIsPalindrome:bob];
+    NSLog(@"%d : %@", bobIsPalindrome, bob);
+    
+    NSString *kanakanak = @"Kanakanak";
+    BOOL kanakanakIsPalindrome = [self stringIsPalindrome:kanakanak];
+    NSLog(@"%d : %@", kanakanakIsPalindrome, kanakanak);
+    
+    NSString *aibohphobia = @"Aibohphobia";
+    BOOL aibohphobiaIsPalindrome = [self stringIsPalindrome:aibohphobia];
+    NSLog(@"%d : %@", aibohphobiaIsPalindrome, aibohphobia);
+    
+   
+    
+    // removing spaces
+    NSString *notAPalindrome = @"this is not a palindrome";
+    BOOL notAPalindromeIsPalindrome = [self stringIsPalindrome:notAPalindrome];
+    NSLog(@"%d : %@", notAPalindromeIsPalindrome, notAPalindrome);
+    
+    NSString *neverOdd = @"never odd or even";
+    BOOL neverOddIsPalindrome = [self stringIsPalindrome:neverOdd];
+    NSLog(@"%d : %@", neverOddIsPalindrome, neverOdd);
+    
+    NSString *iPreferPi = @"I prefer pi";
+    BOOL iPreferPiIsPalindrome = [self stringIsPalindrome:iPreferPi];
+    NSLog(@"%d : %@", iPreferPiIsPalindrome, iPreferPi);
+    
+    
+    
+    // full sentences with punctuation and capitalization
+    NSString *fleeToMe = @"Flee to me, remote elf.";
+    BOOL fleeToMeIsPalindrome = [self stringIsPalindrome:fleeToMe];
+    NSLog(@"%d : %@", fleeToMeIsPalindrome, fleeToMe);
+    
+    NSString *norma = @"Norma is as selfless as I am, Ron.";
+    BOOL normaIsPalindrome = [self stringIsPalindrome:norma];
+    NSLog(@"%d : %@", normaIsPalindrome, norma);
+    
+    NSString *papayaWar = @"No sir! Away! A papaya war is on.";
+    BOOL papayaWarIsPalindrome = [self stringIsPalindrome:papayaWar];
+    NSLog(@"%d : %@", papayaWarIsPalindrome, papayaWar);
+    
+    
+    
     
     // do not alter
     return YES;  //
@@ -22,5 +82,42 @@
  * Implement your methods here.
  
  */
+
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
+    NSString *withoutPunctuation = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" "  withString:@""];
+    
+    NSString *lowercase = [spaceless lowercaseString];
+    
+    NSString *reverse = [self stringByReversingString:lowercase];
+    
+    // Comparison to inspect strings. Removed upon verifying check.
+    //NSLog(@"lowercase: %@, reverse: %@", lowercase, reverse);
+    
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+    NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+    }
+
+    return result;
+}
 
 @end
