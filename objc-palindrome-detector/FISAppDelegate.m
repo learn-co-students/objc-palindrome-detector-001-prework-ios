957bbb05b27131b2ab8bdd6a16e184bc000a1577
Uskinn <s.nevzor@yahoo.com>
2016-02-24 17:58:24 -0500

    qiwi

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -12,15 +12,52 @@
      * Write your check code here.
      
      */
+        // do not alter
     
-    // do not alter
-    return YES;  //
+    NSString *fleeToMe = @"Flee to me, remote elf.";
+    BOOL fleeToMeIsPalindrome = [self stringIsPalindrome:fleeToMe];
+    NSLog(@"%d : %@", fleeToMeIsPalindrome, fleeToMe);
+    
+    NSString *norma = @"Norma is as selfless as I am, Ron.";
+    BOOL normaIsPalindrome = [self stringIsPalindrome:norma];
+    NSLog(@"%d : %@", normaIsPalindrome, norma);
+    
+    NSString *papayaWar = @"No sir! Away! A papaya war is on.";
+    BOOL papayaWarIsPalindrome = [self stringIsPalindrome:papayaWar];
+    NSLog(@"%d : %@", papayaWarIsPalindrome, papayaWar);
+    
+        
+    
+        return YES;  //
 }   ///////////////
 
 /**
  
+ 
  * Implement your methods here.
  
  */
-
+- (NSString *)stringByReversingString:(NSString *)string {
+     return nil;
+ }
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    
+    NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
+    NSString *withoutPunctuation = [string copy];
+    
+    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+    }
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    NSString *lowercase = [spaceless lowercaseString];
+    
+    NSString *reverse = [self stringByReversingString:lowercase];
+    
+    BOOL stringIsEqualToReverse = [lowercase isEqualToString:reverse];
+    
+    return stringIsEqualToReverse;
+}
 @end
