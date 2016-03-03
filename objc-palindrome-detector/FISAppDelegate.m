efec0a5e3272c6900abc3ec845fe129047191a9d
JeffChiu <jchiu@KCC.local>
2016-03-03 15:47:20 -0800

    solution updated

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -30,6 +30,49 @@
     
 
     
+    // single word, with uppercase
+    NSString *bob = @"Bob";
+    BOOL bobIsPalindrome = [self stringIsPalindrome:bob];
+    NSLog(@"%d : %@", bobIsPalindrome, bob);
+    
+    NSString *kanakanak = @"Kanakanak";
+    BOOL kanakanakIsPalindrome = [self stringIsPalindrome:kanakanak];
+    NSLog(@"%d : %@", kanakanakIsPalindrome, kanakanak);
+    
+    NSString *aibohphobia = @"Aibohphobia";
+    BOOL aibohphobiaIsPalindrome = [self stringIsPalindrome:aibohphobia];
+    NSLog(@"%d : %@", aibohphobiaIsPalindrome, aibohphobia);
+    
+    // multiple words, no punctuation
+    NSString *notAPalindrome = @"this is not a palindrome";
+    BOOL notAPalindromeIsPalindrome = [self stringIsPalindrome:notAPalindrome];
+    NSLog(@"%d : %@", notAPalindromeIsPalindrome, notAPalindrome);
+    
+    NSString *neverOdd = @"never odd or even";
+    BOOL neverOddIsPalindrome = [self stringIsPalindrome:neverOdd];
+    NSLog(@"%d : %@", neverOddIsPalindrome, neverOdd);
+    
+    NSString *iPreferPi = @"I prefer pi";
+    BOOL iPreferPiIsPalindrome = [self stringIsPalindrome:iPreferPi];
+    NSLog(@"%d : %@", iPreferPiIsPalindrome, iPreferPi);
+    
+    //within application:didFinishLaunchingWithOptions:
+    
+    // full sentences with punctuation and capitalization
+    NSString *fleeToMe = @"Flee to me, remote elf.";
+    BOOL fleeToMeIsPalindrome = [self stringIsPalindrome:fleeToMe];
+    NSLog(@"%d : %@", fleeToMeIsPalindrome, fleeToMe);
+    
+    NSString *norma = @"Norma is as selfless as I am, Ron.";
+    BOOL normaIsPalindrome = [self stringIsPalindrome:norma];
+    NSLog(@"%d : %@", normaIsPalindrome, norma);
+    
+    NSString *papayaWar = @"No sir! Away! A papaya war is on.";
+    BOOL papayaWarIsPalindrome = [self stringIsPalindrome:papayaWar];
+    NSLog(@"%d : %@", papayaWarIsPalindrome, papayaWar);
+    
+    
+    
     // do not alter
     return YES;  //
 }   ///////////////
@@ -45,13 +88,20 @@
 
 - (BOOL)stringIsPalindrome:(NSString *)string {
     
+    //Create a new NSArray called punctuations and assign it to an array literal containing the string literals for the six common punctuation characters
+    //Create a new NSString variable called withoutPunctuation and assign to a copy of the string argument variable
+    // iterates over the punctuations array
+    //Within the for loop, create new NSString variable called punctuation and assign it to the subscript of the punctuations array using the current value of i
+    // Still within the for loop, reassign the withoutPunctuation variable to capture a call of the stringByReplacingOccurrencesOfString:withString: method on the withoutPunctuation variable (itself). The first argument should be the punctuation string variable, and the second argument should be an empty string literal (@""):
+    
     NSArray *punctuations = @[ @".", @",", @"!", @"?", @":", @";" ];
     NSString *withoutPunctuation = [string copy];
     
-    for (NSUInteger i = 0; i < [punctuations count]; i++) {
+    for (NSUInteger i =0; i <[punctuations count]; i++) {
         NSString *punctuation = punctuations[i];
         withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
     }
+    
     
     NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
     
