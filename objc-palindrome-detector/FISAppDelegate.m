3002ec460dd9e30764b1a5fb8340fa3c06e2c9ea
priscim <priscim@aol.com>
2016-04-07 20:30:32 -0400

    Done.

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -9,7 +9,18 @@
 
     /**
      
-     * Write your check code here.
+     * Write code here
+     
+     @import <UIKit/UIKit.h>
+     @interface FISAppleDelegate: UIResponder <UIAppleDelegate>
+     @property (strong, nonatomic) UIWindow *window;
+     
+     - (BOOL) stringIsPal: (NSString *) string;
+     - NSString * stringByReverseString: (NSString *) string;
+     
+     - NSString * racecar = @ "racecar";
+     
+     - NSString * bob = @"Bob";
      
      */
     
@@ -20,6 +31,33 @@
 /**
  
  * Implement your methods here.
+ 
+    - (BOOL) stringIsPal: (NSString *) string
+ //   { return NO; }
+ 
+ //   - NSString *) stringByReverseString: (NSString *) string
+ //   {return nil;}
+ 
+ //   NSString * result i = [string length]; i>0; i--)
+ //   {NSUInteger index = i-1;
+ //   unichar c = [string charAtIndex: index
+ //   result = [result stringByAppFormat: @"%c", c];}
+ //   retrun result;}
+ 
+ //   NSString * reverse = [self stringByRevStr: string];
+ //   BOOL stringIsEqualToReverse = [stringIsEqualToString: reverse];
+ //  return stringIsEqualToReverse;}
+ 
+ //   BOOL racecarIsPal = [self stringIsPal: racecar];
+ //   NSLog (@ "%d: %@". racecarIsPal, racecar);
+ 
+ //   BOOL palIsPal = [self stringIsPal: palindrome];
+ //   NSLog (@ "%d: %@, palIsPal, palindrome);
+ 
+    BOOL bobIsPal =[self stringIsPal: bob];
+    NSLog(@ "%d : %@", bobIsPal, bob);
+ 
+ 
  
  */
 
