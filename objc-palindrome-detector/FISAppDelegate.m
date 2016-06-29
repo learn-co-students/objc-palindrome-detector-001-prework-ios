8be867e8f3ce4cf91f45ee55d868b73fafb25b3b
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2016-06-29 12:11:03 -0700

    working on the for loop

--- objc-palindrome-detector/FISAppDelegate.m
+++ objc-palindrome-detector/FISAppDelegate.m
@@ -7,20 +7,33 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-    /**
-     
-     * Write your check code here.
-     
-     */
+   
+    NSString *palindrome = @"palindrome";
+    
+    NSString *reversed = [self stringByReversingString:palindrome];
+    
+    NSLog(@"%@: %@", palindrome, reversed);
+    
     
     // do not alter
     return YES;  //
 }   ///////////////
 
-/**
- 
- * Implement your methods here.
- 
- */
+
+- (BOOL)stringIsPalindrome:(NSString *)string {
+    return NO;
+}
+
+- (NSString *)stringByReversingString:(NSString *)string {
+    
+        NSString *result = @"";
+    
+    for (NSUInteger i = [string length]; i > 0; i--) {
+        NSUInteger index = i - 1;
+        unichar c = [string characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%C", c];
+            return result;
+    }
+}
 
 @end
