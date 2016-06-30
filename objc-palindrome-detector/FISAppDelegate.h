bdbf135f1c11397b7dbe88fdd9aa51d2d75f0c66
OliviaGunton <olivia.gunton@gmail.com>
2016-06-30 12:51:18 -0400

    Add and test methods to detect palindromes

--- objc-palindrome-detector/FISAppDelegate.h
+++ objc-palindrome-detector/FISAppDelegate.h
@@ -6,10 +6,15 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+
+//Return YES if string is equal to its reverse, NO otherwise.
+- (BOOL)stringIsPalindrome:(NSString *)string;
+
+//Reverse the characters in string and return the resulting string.
+- (NSString *)stringByReversingString:(NSString *)string;
+
+//Strip punctuation characters and spaces from string and return the resulting string.
+- (NSString *)removeUnwantedChars:(NSString *)string;
+
 
 @end
